0de954778daa1fe1d740b8bed1bb44e47d3d220a
James Bridge <naetharu@gmail.com>
2020-07-21 13:20:29 +0100

    Simplifed PWReset

--- Work Scripts/PWReset.ps1
+++ Work Scripts/PWReset.ps1
@@ -1,9 +1,6 @@
 #1 Import the System.Web type from MS.net - we are going to use this to generate our passwords
 Add-Type -AssemblyName 'System.Web'
 
-#1.1 Create arrays to store logs
-$failLog = @()
-
 #2 Loop with break condition.
 while($true){
     #2 Ask the engineer for the user account name
@@ -28,63 +25,17 @@ while($true){
 #Write-Host "Break on through to the other side!"
 
 #3 Locate the user in AD
-try{
-    if(Get-ADUser -Filter "(Givenname -eq '$name') -and (Surname -eq '$surname')"){
-        $userAccount = Get-ADUser -Filter "(Givenname -eq '$name') -and (Surname -eq '$surname')"
-    }else {
-        Write-Host "User does not exist in AD"
-        $fail = [PSCustomObject]@{
-            Name = "$name $surname"
-            Reason = "User does not exist in AD"
-        }
-
-        $failLog += $fail
-
-        Write-Host "`The process has failed: `n" -ForegroundColor Red
-        $faillog | Format-Table -AutoSize
-        $faillog | Export-Csv -Path "C:\Users\Administrator\Desktop\PWreset\results\fail.csv"
-        return
-    }
-}catch{
-    $fail = [PSCustomObject]@{
-        Name = "$name $surname"
-        Reason = "Failed attempting to get AD account"
-    }
-    $failLog += $fail
-
-    Write-Host "`The process has failed: `n" -ForegroundColor Red
-    $faillog | Format-Table -AutoSize
-    $faillog | Export-Csv -Path "C:\Users\Administrator\Desktop\PWreset\results\fail.csv"
-    return
-}
+$userAccount = Get-ADUser -Filter "(Givenname -eq '$name') -and (Surname -eq '$surname')"
 
 #4 Reset the users password
 $length = 10
 $nonAlphaChars = 2
 $password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
 
-try{
-    Set-ADAccountPassword -Identity $userAccount -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) -ChangePasswordAtLogon $true | Enable-ADAccount
-}catch{
-    Write-Host "User does not exist in AD"
-        $fail = [PSCustomObject]@{
-            Name = "$name $surname"
-            Reason = "Attempt to reset password failed"
-        }
-        $failLog += $fail
 
-        Write-Host "`The process has failed: `n" -ForegroundColor Red
-        $faillog | Format-Table -AutoSize
-        $faillog | Export-Csv -Path "C:\Users\Administrator\Desktop\PWreset\results\fail.csv"
-        return
-}
+Set-ADAccountPassword -Identity $userAccount -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) -ChangePasswordAtLogon $true | Enable-ADAccount
 
 #5 Print results
 Write-Host "`The password has been changed successfully: `n" -ForegroundColor Red
 Write-Host "The new password is: "$password
 $password | Export-Csv "C:\Users\Administrator\Desktop\PWreset\results\success.csv"
-
-
-
-
-
