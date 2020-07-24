3829463c95e5c6da09722c3c201faee4ce948d23
James Bridge <naetharu@gmail.com>
2020-07-24 21:46:21 +0100

    create access rights selection in for add calendar permission

--- Work Scripts/FunctionalExchangeOnline.ps1
+++ Work Scripts/FunctionalExchangeOnline.ps1
@@ -20,7 +20,29 @@ function addCalendarPermission {
     $hostUser = Read-Host "Enter the email address for the primary account: "
     $guestUser = Read-Host "Enter the email address for the the delegate you wish to add as a delegate: "
 
-    Add-MailboxFolderPermission -Identity $($hostUser + ":\Calendar") -User $guestUser -AccessRights Editor -SharingPermissionFlags Delegate
+    Write-Host "Please Select the access level you wish to provide: "
+
+    Write-Host "[1] AvailabilityOnly: View only availability data"
+    Write-Host "[2] LimitedDetails: View availability data with subject and location"
+    Write-Host "[3] Contributor: CreateItems, FolderVisible"
+    Write-Host "[4] Editor: CreateItems, DeleteAllItems, DeleteOwnedItems, EditAllItems, EditOwnedItems, FolderVisible, ReadItems"
+    Write-Host "[5] Custom level - only use this if you know what you are doing!"
+
+    $answer = Read-Host "Please make your selection []:"
+
+    $accessLevel = "AvailabilityOnly"
+
+    # Default to lowest security level
+    switch ($answer) {
+        1 { $accessLevel = "AvailabilityOnly"; break }
+        2 { $accessLevel = "LimitedDetails"; break }
+        3 { $accessLevel = "Contributor"; break }
+        4 { $accessLevel = "Editor"; break }
+        5 { Read-Host "Enter custom value []"; break }
+        default { $accessLevel = "AvailabilityOnly"; break }
+    }
+
+    Add-MailboxFolderPermission -Identity $($hostUser + ":\Calendar") -User $guestUser -AccessRights $accessLevel -SharingPermissionFlags Delegate -WhatIf
 }
 
 function calendarPermissions {
